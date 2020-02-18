d036591adf14949c1f9c84b4de84c25f9f460cd1
Gkemper1989 <60392840+Gkemper1989@users.noreply.github.com>
2020-02-18 14:22:41 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var myAlphabet;
+myAlphabet = ['A','B','C','D','E','F','G'];
+console.log(myAlphabet.length);
+
+function myAlphabetLength () {console.log(myAlphabet.length)};
+myAlphabetLength();
+
+var Planets;
+Planets = ['Mercury','Venus','Earth','Mars','Jupiter'];
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+var wowDataTypes;
+wowDataTypes = [13,'vulture',true];
+console.log(wowDataTypes);
+
+var hello = function() {
+    return 'Hello world!';
+};
+var result = hello();
+console.log(result);
